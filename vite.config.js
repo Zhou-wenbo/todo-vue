d38a2821e454aa0affe04d8e92dce7c6aa0c7842
Zhou-wenbo <1163555037@qq.com>
2026-04-02 19:29:16 +0800

    chore:更新依赖和配置

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [vue()],
+  base: '/todo-vue/'   // 注意：这个名称必须和你的 GitHub 仓库名完全一致
 })
